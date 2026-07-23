8370e2808d65b64f85d82646326151889228c0e4
Vijay Bhaskar Reddy <192425155.simats@saveetha.com>
2026-07-23 19:54:11 +0530

    Delete CO1 directory

--- CO1/Armstrong.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Main{
-public static void main(String[]args){
-Scanner sc=new Scanner(System.in);
-int n=sc.nextInt(),t=n,sum=0;
-while(t>0){int d=t%10;sum+=d*d*d;t/=10;}
-System.out.println(sum==n?"Armstrong Number":"Not an Armstrong Number");
-}
-}
